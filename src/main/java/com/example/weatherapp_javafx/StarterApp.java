83460c552830255b365852d8ad061597fe02c810
Katarzyna Smoliniec <katarzyna.smoliniec.programista@gmail.com>
2023-12-29 14:12:09 +0100

    Add StartetApp and configuration pom.xml

---
+++ src/main/java/com/example/weatherapp_javafx/StarterApp.java
@@ -0,0 +1,9 @@
+package com.example.weatherapp_javafx;
+
+import com.example.weatherapp_javafx.Launcher;
+
+public class StarterApp {
+    public static void main(String[] args) {
+        Launcher.main(args);
+    }
+}
